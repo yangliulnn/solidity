b1147380c05b12c7778e103ee1d9856f2b22046f
杨柳 <yangliulnn@163.com>
2020-05-23 10:54:09 +0800

    Update airdrop.sol

--- airdrop.sol
+++ airdrop.sol
@@ -6,7 +6,14 @@ interface ERC20 {
 
 contract Airdrop {
     ERC20 token;
+    address owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+    
     function airdrop(address tokenAddress, address[] addresses, uint256 amount) public {
+        if (msg.sender != owner) return;
         token = ERC20(tokenAddress);
         for(uint i = 0; i < addresses.length; i++) {
             token.transferFrom(msg.sender, addresses[i], amount);
